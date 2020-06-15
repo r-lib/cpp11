781b5e80732eeb461a44e6b6d39eddd4b2a4a6af
Jim Hester <james.f.hester@gmail.com>
2020-06-15 08:20:50 -0400

    Actually commit matrix.cpp

---
+++ cpp11test/src/matrix.cpp
@@ -0,0 +1,69 @@
+#include "cpp11/matrix.hpp"
+#include "Rmath.h"
+#include "cpp11/doubles.hpp"
+using namespace cpp11;
+namespace writable = cpp11::writable;
+
+[[cpp11::export]] SEXP gibbs_cpp(int N, int thin) {
+  writable::doubles_matrix mat(N, 2);
+  double x = 0, y = 0;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < thin; j++) {
+      x = Rf_rgamma(3., 1. / double(y * y + 4));
+      y = Rf_rnorm(1. / (x + 1.), 1. / sqrt(2. * (x + 1.)));
+      // REprintf("x: %f y: %f\n", x, y);
+    }
+    mat[i][0] = x;
+    mat[i][1] = y;
+  }
+  return mat;
+}
+
+[[cpp11::export]] cpp11::doubles_matrix gibbs_cpp2(int N, int thin) {
+  writable::doubles_matrix mat(N, 2);
+  double x = 0, y = 0;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < thin; j++) {
+      x = Rf_rgamma(3., 1. / double(y * y + 4));
+      y = Rf_rnorm(1. / (x + 1.), 1. / sqrt(2. * (x + 1.)));
+    }
+    mat(i, 0) = x;
+    mat(i, 1) = y;
+  }
+  return mat;
+}
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+[[cpp11::export]] NumericMatrix gibbs_rcpp(int N, int thin) {
+  NumericMatrix mat(N, 2);
+  double x = 0, y = 0;
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < thin; j++) {
+      x = rgamma(1, 3, 1 / (y * y + 4))[0];
+      y = rnorm(1, 1 / (x + 1), 1 / sqrt(2 * (x + 1)))[0];
+    }
+    mat(i, 0) = x;
+    mat(i, 1) = y;
+  }
+
+  return (mat);
+}
+
+[[cpp11::export]] NumericMatrix gibbs_rcpp2(int N, int thin) {
+  NumericMatrix mat(N, 2);
+  double x = 0, y = 0;
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < thin; j++) {
+      x = Rf_rgamma(3., 1. / (y * y + 4));
+      y = Rf_rnorm(1. / (x + 1), 1 / sqrt(2 * (x + 1)));
+    }
+    mat(i, 0) = x;
+    mat(i, 1) = y;
+  }
+
+  return (mat);
+}
---
+++ inst/include/cpp11/matrix.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "cpp11/integers.hpp"
+#include "cpp11/vector.hpp"
+
+namespace cpp11 {
+template <typename V, typename T>
+class matrix {
+ private:
+  V vector_;
+
+  class row {
+   private:
+    matrix& parent_;
+    int row_;
+
+   public:
+    row(matrix& parent, R_xlen_t row) : parent_(parent), row_(row) {}
+    T operator[](const int pos) { return parent_.vector_[row_ + (pos * parent_.nrow_)]; }
+  };
+  friend row;
+
+  int nrow_;
+
+ public:
+  matrix(SEXP data) : vector_(data), nrow_(INTEGER_ELT(vector_, 0)) {}
+
+  template <typename V2, typename T2>
+  matrix(const cpp11::matrix<V2, T2>& rhs) : vector_(rhs) {}
+
+  matrix(int nrow, int ncol) : vector_(R_xlen_t(nrow * ncol)), nrow_(nrow) {
+    vector_.attr("dim") = {nrow, ncol};
+  }
+
+  int nrow() { return nrow_; }
+
+  int ncol() const { return size() / nrow_; }
+
+  SEXP data() const { return vector_.data(); }
+
+  R_xlen_t size() const { return vector_.size(); }
+
+  operator SEXP() const { return SEXP(vector_); }
+
+  // operator sexp() { return sexp(vector_); }
+
+  sexp attr(const char* name) const { return vector_.attr(name); }
+
+  sexp attr(const std::string& name) const { return vector_.attr(name); }
+
+  sexp attr(SEXP name) const { return vector_.attr(name); }
+
+  vector<string> names() const { return vector_.names(); }
+
+  row operator[](const int pos) { return {*this, pos}; }
+
+  T operator()(int row, int col) { return vector_[row + (col * nrow_)]; }
+
+  // operator cpp11::matrix<V, T>() { return SEXP(); }
+};
+
+using doubles_matrix = matrix<vector<double>, double>;
+using integers_matrix = matrix<vector<int>, int>;
+using logicals_matrix = matrix<vector<Rboolean>, Rboolean>;
+using strings_matrix = matrix<vector<string>, string>;
+
+namespace writable {
+using doubles_matrix = matrix<vector<double>, vector<double>::proxy>;
+using integers_matrix = matrix<vector<int>, vector<int>::proxy>;
+using logicals_matrix = matrix<vector<Rboolean>, vector<Rboolean>::proxy>;
+using strings_matrix = matrix<vector<string>, vector<string>::proxy>;
+}  // namespace writable
+
+// TODO: Add tests for Matrix class
+
+};  // namespace cpp11
