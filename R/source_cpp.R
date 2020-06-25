c4a5d344017f557dc05dc05edc1c78d42b70c3b7
Jeroen Ooms <jeroenooms@gmail.com>
2020-06-25 22:39:27 +0200

    Fix for source_cpp on Windows

--- R/source_cpp.R
+++ R/source_cpp.R
@@ -97,7 +97,7 @@ generate_include_paths <- function(packages) {
   for (i in seq_along(packages)) {
     path <- system.file(package = packages[[i]], "include")
     if (is_windows()) {
-      path <- shortPathName(path)
+      path <- shQuote(shortPathName(path))
     }
     out[[i]] <- paste0("-I", path)
   }
