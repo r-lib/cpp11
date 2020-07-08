731a8add7c3fe191f6dc6869a9eb779fe3fac662
Jim Hester <james.f.hester@gmail.com>
2020-07-08 16:23:20 -0400

    Remove references to cpp11test for initial release

--- tests/testthat/test.R
+++
@@ -1,3 +0,0 @@
-if (!covr::in_covr() && get("requireNamespace")("cpp11test", quietly = TRUE) && nzchar(system.file("tests", package = "cpp11test"))) {
-  cpp11test::run_tests(reporter = testthat::default_reporter())
-}
