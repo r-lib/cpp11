c764e4fa18be44a3286ade81bf1b0fbd84ff32c5
Jim Hester <james.f.hester@gmail.com>
2020-08-06 15:04:51 -0400

    Be a little safer when unprotecting
    An API call could error before we get to the PROTECT, so we need to
    track the number of active protections

--- inst/include/cpp11/strings.hpp
+++ inst/include/cpp11/strings.hpp
@@ -125,21 +125,23 @@ inline r_vector<r_string>::r_vector(std::initializer_list<named_arg> il)
     : cpp11::r_vector<r_string>(safe[Rf_allocVector](STRSXP, il.size())),
       capacity_(il.size()) {
   protect_ = protect_sexp(data_);
+  int n_protected = 0;
 
   try {
     unwind_protect([&] {
       Rf_setAttrib(data_, R_NamesSymbol, Rf_allocVector(STRSXP, capacity_));
       SEXP names = PROTECT(Rf_getAttrib(data_, R_NamesSymbol));
+      ++n_protected;
       auto it = il.begin();
       for (R_xlen_t i = 0; i < capacity_; ++i, ++it) {
         SET_STRING_ELT(data_, i, STRING_ELT(it->value(), 0));
         SET_STRING_ELT(names, i, Rf_mkCharCE(it->name(), CE_UTF8));
       }
-      UNPROTECT(1);
+      UNPROTECT(n_protected);
     });
   } catch (const unwind_exception& e) {
     release_protect(protect_);
-    UNPROTECT(1);
+    UNPROTECT(n_protected);
     throw e;
   }
 }
