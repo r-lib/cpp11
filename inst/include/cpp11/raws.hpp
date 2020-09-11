8164d08afa4b6a5406f215fb32a17c142ce975cc
Benjamin Kietzman <bengilgit@gmail.com>
2020-09-11 15:32:18 -0400

    Update inst/include/cpp11/raws.hpp

--- inst/include/cpp11/raws.hpp
+++ inst/include/cpp11/raws.hpp
@@ -8,7 +8,7 @@
 #include "cpp11/R.hpp"                // for RAW, SEXP, SEXPREC, Rf_allocVector
 #include "cpp11/attribute_proxy.hpp"  // for attribute_proxy
 #include "cpp11/named_arg.hpp"        // for named_arg
-#include "cpp11/protect.hpp"          // for protect_sexp, preserved.release
+#include "cpp11/protect.hpp"          // for preserved
 #include "cpp11/r_vector.hpp"         // for r_vector, r_vector<>::proxy
 #include "cpp11/sexp.hpp"             // for sexp
 
