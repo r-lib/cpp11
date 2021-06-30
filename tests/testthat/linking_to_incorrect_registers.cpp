ab20cd2ab2c3a9b819f3d891fc5adba01cb546b1
Shelby Bearrows <39840820+sbearrows@users.noreply.github.com>
2021-06-30 14:17:27 -0600

    Bug cpp11 decorators (#197)
    Co-authored-by: Jim Hester <james.f.hester@gmail.com>

---
+++ tests/testthat/linking_to_incorrect_registers.cpp
@@ -0,0 +1,12 @@
+[[cpp11::link_to("progress")]]
+
+[[cpp11::register]] void
+show_progress() {
+  RProgress::RProgress pb("Processing [:bar] ETA: :eta");
+
+  pb.tick(0);
+  for (int i = 0; i < 100; i++) {
+    usleep(2.0 / 100 * 1000000);
+    pb.tick();
+  }
+}
