7c1c107088a100a527f65901173860394d438793
Jim Hester <james.f.hester@gmail.com>
2021-07-24 11:22:48 -0400

    Remove extraneous file

--- tests/testthat/cpp11.cpp
+++
@@ -1,9 +0,0 @@
-#include "cpp11/declarations.hpp"
-using namespace cpp11;
-// single_27.cpp
-int foo();
-extern "C" SEXP _single_27_foo() {
-  BEGIN_CPP11
-  return cpp11::as_sexp(foo());
-  END_CPP11
-}
