00690478e6cc04e75d56bfb26866e04d297dfc58
Jim Hester <james.f.hester@gmail.com>
2021-09-17 14:52:23 -0400

    Fix formatting

--- cpp11test/src/errors_fmt.cpp
+++ cpp11test/src/errors_fmt.cpp
@@ -6,18 +6,12 @@ using namespace cpp11;
 [[cpp11::register]] void my_stop(std::string mystring, int myarg) {
   cpp11::stop(mystring, myarg);
 }
-[[cpp11::register]] void my_stop_n1(std::string mystring) {
-  cpp11::stop(mystring);
-}
+[[cpp11::register]] void my_stop_n1(std::string mystring) { cpp11::stop(mystring); }
 [[cpp11::register]] void my_warning(std::string mystring, std::string myarg) {
   cpp11::warning(mystring, myarg);
 }
-[[cpp11::register]] void my_warning_n1(std::string mystring) {
-  cpp11::warning(mystring);
-}
+[[cpp11::register]] void my_warning_n1(std::string mystring) { cpp11::warning(mystring); }
 [[cpp11::register]] void my_message(std::string mystring, std::string myarg) {
   cpp11::message(mystring, myarg);
 }
-[[cpp11::register]] void my_message_n1(std::string mystring) {
-  cpp11::message(mystring);
-}
+[[cpp11::register]] void my_message_n1(std::string mystring) { cpp11::message(mystring); }
