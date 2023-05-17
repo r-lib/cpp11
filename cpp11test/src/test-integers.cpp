3c877986ce886b320ceafb853358d856f07834f4
Davis Vaughan <davis@rstudio.com>
2023-05-17 06:13:58 -0400

    Fix missing value propagation in `as_integers/doubles()` (#265)
    * Correctly propagate missing values in `as_integers()`
    
    Also:
    - Switch to `R_xlen_t` over `size_t` since this is what `size()` returns and is what our constructors prefer
    - Initialize return value with `ret(len)` since that seems cleaner
    - Forward declare required bits from `doubles.hpp`. Use of `is_na(<dbl>)` is new, so it makes sense that we need to forward declare it. Even before this PR, we used the `[]` double operator, so you might be wondering why we need the forward declaration. Well, it only previously worked by luck because we had a hidden dependency on `cpp11/doubles.hpp` in `test-integers.cpp` by including `cpp11/doubles.hpp` before `cpp11/integers.hpp`. I swapped them around in the test file and it indeed failed without this forward declaration.
    
    * Correctly propagate missing values in `as_doubles()`
    
    Also:
    - Switch to `R_xlen_t` over `size_t` since this is what `size()` returns and is what our constructors prefer
    - Initialize sized return value, rather than using `push_back()`, which should be faster
    - Forward declare required bits from `integers.hpp`. There is no `is_na(<int>)` specialization to forward declare, but there is a `na<int>()` forward declaration that is required, because that is used by the default `is_na()` implementation
    - Move double specializations of `na()` and `is_na()` before the implementation of `as_doubles()`. Since `as_doubles()` now calls the `na<double>()` specialization, it has to be implemented (or forward declared) ahead of time.
    
    * NEWS bullet
    
    * Guess at how to make the clang formatter happy
    
    * Separate `cpp11/integers.hpp` into its own include "block"
    
    * Typo, `convertable` -> `convertible`
    
    ---------
    
    Co-authored-by: Romain François <romain@rstudio.com>

--- cpp11test/src/test-integers.cpp
+++ cpp11test/src/test-integers.cpp
@@ -1,7 +1,9 @@
 #include "Rversion.h"
-#include "cpp11/doubles.hpp"
+
 #include "cpp11/function.hpp"
 #include "cpp11/integers.hpp"
+
+#include "cpp11/doubles.hpp"
 #include "cpp11/strings.hpp"
 
 #include <testthat.h>
@@ -36,6 +38,11 @@ context("integers-C++") {
     expect_true(t[2] == 100000);
     expect_true(t[3] == 100000);
     expect_true(TYPEOF(t) == INTSXP);
+
+    cpp11::writable::doubles na;
+    na.push_back(cpp11::na<double>());
+    cpp11::integers na2(cpp11::as_integers(na));
+    expect_true(cpp11::is_na(na2[0]));
   }
 
   test_that("integers.push_back()") {
