cd873a213ea7a966dc6fc80887d381b7c0a00f9e
Romain François <romain@posit.co>
2023-05-23 12:47:22 +0200

    + expect_true(ISNA(na2[0]))

--- cpp11test/src/test-doubles.cpp
+++ cpp11test/src/test-doubles.cpp
@@ -397,6 +397,7 @@ context("doubles-C++") {
     cpp11::writable::integers na;
     na.push_back(cpp11::na<int>());
     cpp11::doubles na2(cpp11::as_doubles(na));
+    expect_true(ISNA(na2[0]));
     expect_true(cpp11::is_na<double>(na2[0]));
   }
 
