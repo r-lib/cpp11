f49997748d99f9aea2bf8b7c02cc63d4ba54767b
Davis Vaughan <davis@posit.co>
2024-08-22 17:21:21 -0400

    Remove implicit conversion operators on `sexp`

--- cpp11test/src/test-function.cpp
+++ cpp11test/src/test-function.cpp
@@ -5,22 +5,22 @@
 context("function-C++") {
   test_that("functions can be called") {
     auto median = cpp11::package("stats")["median"];
-    double res = median(cpp11::as_sexp({1., 2., 3., NA_REAL}), true);
-    expect_true(res == 2.);
+    cpp11::sexp res = median(cpp11::as_sexp({1., 2., 3., NA_REAL}), true);
+    expect_true(cpp11::as_cpp<double>(res) == 2.);
 
-    double res2 = median(cpp11::as_sexp({1., 2., 3., NA_REAL}), false);
-    expect_true(ISNAN(res2));
+    cpp11::sexp res2 = median(cpp11::as_sexp({1., 2., 3., NA_REAL}), false);
+    expect_true(ISNAN(cpp11::as_cpp<double>(res2)));
   }
 
   test_that("functions can be called with named arguments") {
     using namespace cpp11::literals;
 
     auto median = cpp11::package("stats")["median"];
-    double res = median("x"_nm = {1., 2., 3., NA_REAL}, "na.rm"_nm = true);
-    expect_true(res == 2.);
+    cpp11::sexp res = median("x"_nm = {1., 2., 3., NA_REAL}, "na.rm"_nm = true);
+    expect_true(cpp11::as_cpp<double>(res) == 2.);
 
-    double res2 = median(cpp11::as_sexp({1., 2., 3., NA_REAL}), false);
-    expect_true(ISNAN(res2));
+    cpp11::sexp res2 = median(cpp11::as_sexp({1., 2., 3., NA_REAL}), false);
+    expect_true(ISNAN(cpp11::as_cpp<double>(res2)));
   }
 
   test_that("base functions can be called") {
@@ -29,10 +29,9 @@ context("function-C++") {
     auto close = cpp11::package("base")["close"];
 
     cpp11::sexp con = file("foo");
+    cpp11::sexp res = isOpen(con);
 
-    bool res = isOpen(con);
-
-    expect_true(res == false);
+    expect_true(cpp11::as_cpp<bool>(res) == false);
 
     close(con);
   }
