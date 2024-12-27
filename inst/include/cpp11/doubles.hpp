713f14771f72e526f29c9042671a056eaa2d5810
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-12-27 03:34:57 -0500

    bool -> int + using transform for bool -> double

--- inst/include/cpp11/doubles.hpp
+++ inst/include/cpp11/doubles.hpp
@@ -8,6 +8,7 @@
 #include "cpp11/R.hpp"         // for SEXP, SEXPREC, Rf_allocVector, REAL
 #include "cpp11/as.hpp"        // for as_sexp
 #include "cpp11/protect.hpp"   // for safe
+#include "cpp11/r_bool.hpp"    // for r_bool
 #include "cpp11/r_vector.hpp"  // for vector, vector<>::proxy, vector<>::...
 #include "cpp11/sexp.hpp"      // for sexp
 
@@ -71,13 +72,12 @@ typedef r_vector<double> doubles;
 }  // namespace writable
 
 typedef r_vector<int> integers;
+typedef r_vector<r_bool> logicals;
 
 inline doubles as_doubles(SEXP x) {
   if (detail::r_typeof(x) == REALSXP) {
     return doubles(x);
-  }
-
-  else if (detail::r_typeof(x) == INTSXP) {
+  } else if (detail::r_typeof(x) == INTSXP) {
     integers xn(x);
     size_t len = xn.size();
     writable::doubles ret(len);
@@ -85,14 +85,13 @@ inline doubles as_doubles(SEXP x) {
       return value == NA_INTEGER ? NA_REAL : static_cast<double>(value);
     });
     return ret;
-  }
-
-  else if (detail::r_typeof(x) == LGLSXP) {
-    size_t len = Rf_xlength(x);
+  } else if (detail::r_typeof(x) == LGLSXP) {
+    logicals xn(x);
+    size_t len = xn.size();
     writable::doubles ret(len);
-    for (size_t i = 0; i < len; ++i) {
-      ret[i] = LOGICAL(x)[i] == NA_LOGICAL ? NA_REAL : static_cast<double>(LOGICAL(x)[i]);
-    }
+    std::transform(xn.begin(), xn.end(), ret.begin(), [](bool value) {
+      return value == NA_LOGICAL ? NA_REAL : static_cast<double>(value);
+    });
     return ret;
   }
 
